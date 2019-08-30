30dd73671d5eb537f5d9bace1295cdb0d55ab852
nick <nickjobszh@gmail.com>
2019-08-30 08:22:35 +0200

    fix bin

--- src/console.php
+++ src/console.php
@@ -3,8 +3,8 @@
 
 declare(strict_types=1);
 
-if (is_file(__DIR__ . '/../autoload.php') === true) {
-    include_once __DIR__ . '/../autoload.php';
+if (is_file(__DIR__ . '/../../../autoload.php') === true) {
+    include_once __DIR__ . '/../../../autoload.php';
 } else {
     include_once __DIR__ . '/../vendor/autoload.php';
 }
